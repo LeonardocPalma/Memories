40a63173d06b4b52c10c16042749351899e9db41
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-15 22:35:02 -0300

    feat: config routes and memory controler

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -1,7 +1,28 @@
 const Memory = require("../models/Memory");
 
 const createMemory = async (req, res) => {
-  res.json("Deu certo");
+  try {
+    const { title, description } = req.body;
+
+    const src = `image/${req.file.filename}`;
+
+    if (!title || !description) {
+      return res.status(400).send("Todos os campos são obrigatórios!");
+    }
+
+    const newMemory = new Memory({
+      title,
+      src,
+      description,
+    });
+
+    await newMemory.save();
+
+    res.json({ msg: "Memória criada com sucesso", newMemory });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Ocorreu um erro!");
+  }
 };
 
 module.exports = {
--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,23 @@ const router = express.Router();
 
 const { createMemory } = require("./controllers/MemoryController");
 
-router.post("/", (req, res) => createMemory(req, res));
+const upload = require("./helpers/upload");
+
+router.post(
+  "/",
+  upload.single("image"),
+  (req, res, next) => {
+    const image = req.file;
+
+    if (!image) {
+      return res
+        .status(400)
+        .json({ error: "Nenhuma imagem enviada, por favor envie um!" });
+    }
+
+    next();
+  },
+  (req, res) => createMemory(req, res)
+);
 
 module.exports = router;
