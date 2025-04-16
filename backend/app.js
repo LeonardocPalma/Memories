e2c068edd33ab269eea061069208c1e10923e070
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-15 21:16:52 -0300

    feat: First commit, initializing project from backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const port = 3000
+
+app.listen(port, async () => {
+    console.log(`O servidor inicio na porta: ${port}`);
+});
