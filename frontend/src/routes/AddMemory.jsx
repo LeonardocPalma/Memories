8ff8440090db7afa05be2b86ccf9cfba257db337
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-18 20:06:10 -0300

    feat:codding css from AddMemory

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import "./AddMemory.css";
+
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
 
 const AddMemory = () => {
   return (
-    <div>AddMemory</div>
-  )
-}
+    <div className="add-memory-page">
+      <h2>Crie uma nova memória</h2>
+      <form>
+        <label>
+          <p>Título:</p>
+          <input type="text" placeholder="Defina um título" name="title" />
+        </label>
+        <label>
+          <p>Descrição:</p>
+          <textarea
+            name="description"
+            placeholder="Descreva sua memória..."
+          ></textarea>
+        </label>
+        <label>
+          <p>Foto:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" className="btn" value="Criar memória" />
+      </form>
+    </div>
+  );
+};
 
-export default AddMemory
+export default AddMemory;
