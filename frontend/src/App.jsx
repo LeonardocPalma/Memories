0a71deec0b6df204f48d781aef5eda7d2e742d97
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-18 19:19:02 -0300

    feat:configured react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
-
-
   return (
     <div className="app">
       <h1>Memories</h1>
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
