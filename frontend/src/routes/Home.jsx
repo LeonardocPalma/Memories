32830b9767e5c392fd515d2b87842e6b72bca5b5
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-18 20:45:28 -0300

    feat:configured page home and css

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import "./Home.css";
+
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "../axios-config";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [memories, setMemories] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const getMemories = async () => {
+      try {
+        const response = await axios.get("/memories");
+        setMemories(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getMemories();
+  }, []);
+
+  return (
+    <div className="home">
+      <h2>Confira as nossas memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 &&
+          memories.map((memory) => (
+            <div className="memory" key={memory._id}>
+              <img
+                src={`${axios.defaults.baseURL}${memory.src}`}
+                alt={memory.title}
+              />
+              <p>{memory.title}</p>
+              <Link to={`/memories/${memory._id}`} className="btn">
+                Comentar
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
